523742829e4e1a835141289cc27cb2a81ea641c8
Stefan Buhrmester <buhrmi@gmail.com>
2022-12-21 21:59:31 +0100

    theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 
   export let data
 
-  const { bg, logo } = data.content
+  const { bg, logo, bg_video_portrait, bg_video_landscape } = data.content
 </script>
 
 <svelte:head>
@@ -12,9 +12,20 @@
   <link rel="icon" href="API_URL/attachments/{data.content.favicon}" />
 </svelte:head>
 
-
 {#if bg}
   <div class="fixed -z-1 w-full h-screen bg-no-repeat bg-cover bg-center" style:background-image="url(API_URL/attachments/{bg}?w=1920)" />
+{/if}
+
+{#if bg_video_portrait}
+  <video autoplay md:hidden muted loop playsinline class="fixed -z-1 w-full h-screen object-cover">
+    <source src="API_URL/attachments/{video}" type="video/mp4" />
+  </video>
+{/if}
+
+{#if bg_video_landscape}
+  <video autoplay hidden md:block muted loop playsinline class="fixed -z-1 w-full h-screen object-cover">
+    <source src="API_URL/attachments/{video}" type="video/mp4" />
+  </video>
 {/if}
 
 <div class="h-screen flex flex-col items-center justify-center">
